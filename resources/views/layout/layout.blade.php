a18d26d6fa15a0d77e2f9bf24f5a62a51dc07289
Kyaw Zaww <kzaww4422@gmail.com>
2024-02-15 13:56:39 +0630

    1502changeuserguide

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -67,7 +67,7 @@
                 @endif
 
                 <li class="sidebar_items" onclick="this.childNodes[1].click()">
-                    <a href="{{ asset('image/Received goods .pdf') }}" target="_blank" id="user_guide" hidden></a>
+                    <a href="{{ asset('image/receive_goods_userguide.pdf') }}" target="_blank" id="user_guide" hidden></a>
                     <div class="sidebar_text">
                         <i class='bx bxs-book-content' style="font-size: 2rem;margin: 10px 0 0 10px;"></i>
                         <span>User's Guide</span>
