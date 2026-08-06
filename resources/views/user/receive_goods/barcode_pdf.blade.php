f738f8afc0fd658f7ab58ba531eed360564f2ca4
WutYeePhyoPro1 <wutyeephyo.pro1@gmail.com>
2026-08-06 15:17:06 +0630

    barcode pdf fixed

--- resources/views/user/receive_goods/barcode_pdf.blade.php
+++ resources/views/user/receive_goods/barcode_pdf.blade.php
@@ -11,19 +11,29 @@
         .label { position: absolute; width: 34mm; height: 20.49mm; padding: .7mm 1mm; overflow: hidden; }
         .bar2 .label { height: 10.245mm; padding: .25mm 1mm; }
         .name { height: 7mm; overflow: hidden; font-size: 7pt; line-height: 8pt; font-weight: 700; }
+        .bar1 .name { height: 5.5mm; }
+        .name.long { font-size: 6pt; line-height: 6.5pt; }
+        .name.very-long { font-size: 5.2pt; line-height: 5.7pt; }
         .bar2 .name { height: 2.5mm; font-size: 4.2pt; line-height: 4.5pt; }
+        .bar2 .name.long { font-size: 3.7pt; line-height: 4pt; }
+        .bar2 .name.very-long { font-size: 3.2pt; line-height: 3.5pt; }
         .barcode { width: 31mm; height: 9mm; overflow: hidden; margin: .5mm auto 0; }
         .barcode > div { margin-left: auto; margin-right: auto; }
+        .bar1 .barcode { height: 7.5mm; margin-top: .2mm; }
         .bar2 .barcode { height: 3.8mm; margin-top: .1mm; }
         .code-row { width: 31mm; margin: 0 auto; font-size: 6.5pt; line-height: 7pt; font-weight: 700; white-space: nowrap; }
         .code { display: inline-block; width: 25mm; text-align: center; letter-spacing: .15mm; }
         .unit { float: right; font-size: 5.5pt; }
         .date { margin-top: .5mm; font-size: 5.3pt; line-height: 5.5pt; font-weight: 700; white-space: nowrap; text-align: center; }
+        .bar1 .date { margin-top: .1mm; font-size: 4.8pt; line-height: 5pt; }
         .bar2 .code-row { font-size: 4pt; line-height: 4.2pt; }
         .bar2 .code { width: 25mm; }
         .bar2 .unit, .bar2 .date { margin-top: 0; font-size: 3.7pt; line-height: 4pt; }
         .label.first .barcode, .label.first .code-row { margin-left: 0; margin-right: auto; }
         .label.last .barcode, .label.last .code-row { margin-left: auto; margin-right: 0; }
+        .bar1 .label.first > * { position: relative; left: -.8mm; }
+        .bar1 .label.last > * { position: relative; left: .8mm; }
+        .bar2 .label.last .barcode, .bar2 .label.last .code-row { margin-left: auto; margin-right: auto; }
         .bar3 .name { height: 5mm; font-size: 6pt; line-height: 6.5pt; }
         .bar3 .barcode { height: 7mm; margin-top: .2mm; }
         .bar3 .code-row { font-size: 5.5pt; line-height: 6pt; }
@@ -37,6 +47,8 @@
 @php
     $perPage = $type === 2 ? 6 : 3;
     $items = range(1, $quantity);
+    $nameLength = \Illuminate\Support\Str::length($product->supplier_name);
+    $nameClass = $nameLength > 65 ? 'very-long' : ($nameLength > 45 ? 'long' : '');
 @endphp
 @foreach(array_chunk($items, $perPage) as $page)
     <div class="sheet {{ $type === 2 ? 'bar2' : 'bar' . $type }} {{ $loop->last ? 'last' : '' }}">
@@ -47,11 +59,14 @@
                 $column = $index % 3;
                 $row = $type === 2 ? intdiv($index, 3) : 0;
                 $left = $column * 37.1;
-                $top = 3.217 + ($row * 10.245);
+                if ($type === 2 && $column === 2) {
+                    $left += 1.5;
+                }
+                $top = ($type === 1 ? 1.5 : 3.217) + ($row * 10.245);
                 $edgeClass = $column === 0 ? 'first' : ($column === 2 ? 'last' : 'middle');
             @endphp
             <div class="label {{ $edgeClass }}" style="left: {{ $left }}mm; top: {{ $top }}mm;">
-                <div class="name">{{ \Illuminate\Support\Str::limit($product->supplier_name, 77) }}</div>
+                <div class="name {{ $nameClass }}">{{ \Illuminate\Support\Str::limit($product->supplier_name, 77) }}</div>
                 <div class="barcode">{!! $barcodeHtml !!}</div>
                 <div class="code-row"><span class="code">{{ $product->bar_code }}</span><span class="unit">{{ $product->unit }}</span></div>
                 @if($type === 3)
